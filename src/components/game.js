15cc710ef67583272720138e11892b550356498d
Samuele <samuele.lafleur@eoc.ch>
2020-01-24 16:33:32 +0100

    intallazione redux react e applicazione dell'animazione

--- src/components/game.js
+++ src/components/game.js
@@ -4,24 +4,35 @@ import Button from "@material-ui/core/Button";
 
 const Game = () => {
     const [animatedClass, setAnimatedClass] = useState('');
-    const cubo = (
-        <Button  variant="contained" className={animatedClass} color={"primary"} style={{
-            height : 50,
-            weight : 50,
-            margin : 1
-        }}
-        onClick={()=> setAnimatedClass('animated bounceIn ')}
-        />
-    );
-
-    return(
-      <div style={{color : 'white'}} >
-
-          {cubo}
+    let cubo;
 
 
-Già lo sai
-      </div>
+        cubo = (
+            <Button variant="contained" className={animatedClass} color={"primary"} style={{
+                height: 50,
+                weight: 50,
+                margin: 1
+            }}
+                    onClick={() =>{ setAnimatedClass('');
+                   setAnimatedClass('animated bounceIn')}}
+            />
+        );
+
+    //= (
+    //     <Button variant="contained" className={animatedClass} color={"primary"} style={{
+    //         height: 50,
+    //         weight: 50,
+    //         margin: 1
+    //     }}
+    //             onClick={() => setAnimatedClass('animated bounceIn')}
+    //     />
+    // );
+
+    return (
+        <div style={{color: 'white'}}>
+            {cubo}
+            Già lo sai
+        </div>
     );
 };
 
