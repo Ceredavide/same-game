492f61095f3540b1d7ea2f40d23f76c64ab07bfb
Davide <ceredavide@live.it>
2020-02-06 09:44:36 +0100

    bella

--- src/screens/game.js
+++ src/screens/game.js
@@ -7,11 +7,10 @@ import Cubo from "../components/cubo";
 const useStyles = makeStyles({
     root: {
         padding: 10,
-        height: 275,
-        width: 275
+        height: 350,
+        width: 350
     },
 });
-
 
 const Game = () => {
     const [buttonsState, setButtonsState] = useState([])
