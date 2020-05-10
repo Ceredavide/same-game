681a1055d23b67a586a8028b3bf70642d3a54d39
Davide <ceredavide@live.it>
2020-05-10 20:04:44 +0200

    sku

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import './Css/App.css';
-import {Container} from '@material-ui/core'
+import { Container } from '@material-ui/core'
+
 import Home from "./screens/Home"
-import Game from './screens/game'
+import Game from './screens/Game'
+
+const App = () => {
+    return (
+        <Container>
+            <Router>
+                <div style={styles.container}>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/game" component={Game} />
+                    </Switch>
+                </div>
+            </Router>
+        </Container>
+
+    );
+};
 
 const styles = {
     container: {
@@ -12,7 +30,7 @@ const styles = {
         display: 'flex',
         justifyContent: 'center',
         flexDirection: 'column',
-       marginTop: 200,
+        marginTop: 200,
     },
     startButton: {
         marginTop: 20,
@@ -20,22 +38,6 @@ const styles = {
         height: 50,
 
     }
-};
-
-const App = () => {
-    return (
-        <Container>
-            <Router>
-                <div style={styles.container}>
-                    <Switch>
-                        <Route exact path="/" component={() => <Home/>}/>
-                        <Route exact path="/game" component={() => <Game/>}/>
-                    </Switch>
-                </div>
-            </Router>
-        </Container>
-
-    );
 };
 
 export default App;
