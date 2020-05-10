59e4182ac83779d4fc6454d66adc386d6d3af982
Davide <ceredavide@live.it>
2020-05-10 20:04:18 +0200

    creato state da utilizzare e migliorato stile

--- src/screens/game.js
+++ src/screens/game.js
@@ -1,37 +1,46 @@
-import React, {useState} from "react";
-import {makeStyles} from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 
 import HomeButton from "../components/Buttons/homeButton";
 import RefreshButton from "../components/Buttons/refreshButton";
-import Griglia from "../components/griglia";
-
-const useStyles = makeStyles({
-    root: {
-        padding: 10,
-        height: 360,
-        width: 330,
-        backgroundColor : 'gray'
-
-    },
-    griglia : {
-
-    }
-});
+import Griglia from "../components/Griglia";
+import useColumns from "../hooks/useColumns";
 
 const Game = () => {
-    const [buttonsState, setButtonsState] = useState([]);
+    const [gameCounter, setGameCounter] = useState(0)
+    const [score, setScore] = useState(0)
+
+    const columns = useColumns()
+
+    function onButtonClick(x, y, color) {
+        console.log(x, y, color)
+    }
+
+    console.log(columns)
+
     const classes = useStyles();
     return (
-
         <Card className={classes.root} elevation={3} >
-            <Griglia/>
+            <Griglia columns={columns} onButtonClick={onButtonClick} />
+            {/* Mettere punteggio */}
             <div>
-                <HomeButton/>
+                <HomeButton />
+                <RefreshButton />
             </div>
+
         </Card>
     );
 };
 
+const useStyles = makeStyles({
+    root: {
+        padding: 10,
+        backgroundColor: 'gray'
+    },
+    griglia: {
+
+    }
+});
 
 export default Game;
