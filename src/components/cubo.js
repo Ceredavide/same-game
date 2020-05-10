8d7ee4cb3ef93795c020c5fec631da04f0cb03d5
Davide <ceredavide@live.it>
2020-05-10 20:03:54 +0200

    migliorato componente e gestione state

--- src/components/cubo.js
+++ src/components/cubo.js
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
-const styles = {
-    height: 50,
-    weight: 50,
-    margin: 1
-}
-const Cubo = ({styles}) => {
+
+const Cubo = ({ state, onButtonClick }) => {
     const [animatedClass, setAnimatedClass] = useState('');
 
-    return <Button variant="contained" className={animatedClass} color={"primary"} style={styles}
-        onClick={() => {
-            setAnimatedClass('');
-            setAnimatedClass('animated bounceIn')
-        }}
-    />
+    function animate() {
+        // metterci setCubi
+        setAnimatedClass('');
+        setAnimatedClass('animated bounceIn')
+        onButtonClick(state.x, state.y, state.color)
+    }
+
+    const styles = {
+        height: 50,
+        weight: 50,
+        margin: 1
+    }
+
+    return (
+        <Button
+            variant="contained"
+            className={animatedClass}
+            color={"primary"}
+            style={{ ...styles, backgroundColor: state.color }}
+            onClick={animate}
+        />
+    )
 }
 
 export default Cubo;
