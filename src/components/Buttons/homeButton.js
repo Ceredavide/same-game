2bea8ffd8bb391a87ca459d869114e7165124009
Davide <ceredavide@live.it>
2020-06-09 10:14:47 +0200

    rimosso styles inutilizzati

--- src/components/Buttons/homeButton.js
+++ src/components/Buttons/homeButton.js
@@ -3,11 +3,6 @@ import '../../Css/StartButton.scss'
 import HomeIcon from '@material-ui/icons/Home'
 import {withRouter} from "react-router";
 
-const styles = {
-    width : 50,
-    height : 50,
-};
-
 const HomeButton = ({history}) => {
 
     return (
--- src/components/Buttons/refreshButton.js
+++ src/components/Buttons/refreshButton.js
@@ -1,12 +1,6 @@
 import React from "react";
 import '../../Css/StartButton.scss'
 import RefreshIcon from '@material-ui/icons/Refresh';
-import {withRouter} from "react-router";
-
-const styles = {
-    width : 50,
-    height : 50,
-};
 
 const RefreshButton = ({history}) => {
 
